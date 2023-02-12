7e544be178ca3fae347300411fb53624985d86fb
82476 <82746@proton.me>
2023-02-12 15:07:39 +0200

    fix avg

--- main.py
+++ main.py
@@ -10,15 +10,15 @@ class PlantApp():
         self.__changes_list = []
 
     def print_instructions(self):
-        print("\033[32m\033[2m[c]\033[22;39m Create plant")
-        print("\033[32m\033[2m[d]\033[22;39m Delete plant")
-        print("\033[32m\033[2m[ls]\033[22;39m List plants")
-        print("\033[34m\033[2m[w]\033[22;39m Water plant")
-        print("\033[34m\033[2m[u]\033[22;39m Unwater plant")
-        print("\033[34m\033[2m[lw]\033[22;39m List last times watered")
-        print("\033[35m\033[2m[save]\033[22;39m Save unsaved changes")
-        print("\033[35m\033[2m[reset]\033[22;39m Delete unsaved changes")
-        print("\033[35m\033[2m[q]\033[22;39m Quit")
+        print("\033[32m" + "[c] " + "\033[22;39m" + "Create plant")
+        print("\033[32m" + "[d] " + "\033[22;39m" + "Delete plant")
+        print("\033[32m" + "[ls] " + "\033[22;39m" + "List plants")
+        print("\033[34m" + "[w] " + "\033[22;39m" + "Water plant")
+        print("\033[34m" + "[u] " + "\033[22;39m" + "Unwater plant")
+        print("\033[34m" + "[lw] " + "\033[22;39m" + "List last times watered")
+        print("\033[35m" + "[save] " + "\033[22;39m" + "Save unsaved changes")
+        print("\033[35m" + "[reset] " + "\033[22;39m" + "Delete unsaved changes")
+        print("\033[35m" + "[q] " + "\033[22;39m" + "Quit")
 
     def list_plants(self):
             plant_names = self.__plant_db.get_all_plant_names()
@@ -28,12 +28,17 @@ class PlantApp():
 
     def __plant_avg_watering_interval(self, all_waterings:list):
             # calculate average interval between all waterings
+            waterings = all_waterings.copy()
             interval_sum = 0
-            prev_date = datetime.now().date()
-            for w in all_waterings:
+            prev_date = datetime.fromisoformat(waterings.pop(0)).date()
+            for w in waterings:
                 delta = prev_date - datetime.fromisoformat(w).date()
                 interval_sum += delta.days
-            avg_interval = round(interval_sum / len (all_waterings), 2)
+            try:
+                avg_interval = round(interval_sum / len (waterings), 2)
+            except ZeroDivisionError:
+                avg_interval = None
+
             return avg_interval
 
     def list_last_times_watered(self):
@@ -51,9 +56,17 @@ class PlantApp():
                     delta = curr_date - last_watering_date
                     delta = delta.days
 
-                    print(f"  · {p_name}: \033[4;34m{last_watering_date.strftime('%d.%m.%y')}\033[24;2m {delta} days ago (avg every {avg_interval} days)\033[22;39m")
+                    print(f"  · {p_name}: \033[4;34m{last_watering_date.strftime('%d.%m.%y')}\033[24;2m, {delta} days ago ", end="")
+                    print(f"(times watered: {len(all_waterings)}", end="")
+
+                    if avg_interval != None:
+                        print(f", avg every {avg_interval} days)",end="")
+                    else:
+                        print(")", end="")
+
+                    print("\033[22;39m")
                 else:
-                    print(f"  · {p_name}: \033[4;34m None \033[24;2m None \033[22;39m")
+                    print(f"  · {p_name}: \033[4;34m None \033[24;39m")
             print()
 
 
